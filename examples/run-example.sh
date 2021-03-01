28c83a0b2045db298eea38dde25d1c4b8a097a2e
Jakub Bednar <jakub.bednar@gmail.com>
2021-03-01 15:28:23 +0100

    chore(ci): use latest tag for InfluxDB 2, use correct image for InfluxDB 2 nightly

--- examples/run-example.sh
+++ examples/run-example.sh
@@ -49,7 +49,7 @@ docker run \
        --name influxdb_v2 \
        --network influx_network \
        --publish 8086:8086 \
-       quay.io/influxdb/influxdb:v2.0.4
+       influxdb:latest
 
 #
 # Post onBoarding request to InfluxDB 2
