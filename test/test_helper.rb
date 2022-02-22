474100b4f6a59b0b18cdeb6c13e9ff95002b0aee
Jakub Bednar <jakub.bednar@gmail.com>
2022-02-22 12:35:01 +0100

    chore(ci): use new codecov uploader for reporting code coverage

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -22,8 +22,8 @@ require 'simplecov'
 SimpleCov.start
 
 if ENV['CI'] == 'true'
-  require 'codecov'
-  SimpleCov.formatter = SimpleCov::Formatter::Codecov
+  require 'simplecov-cobertura'
+  SimpleCov.formatter = SimpleCov::Formatter::CoberturaFormatter
 end
 
 $LOAD_PATH.unshift File.expand_path('../lib', __dir__)
